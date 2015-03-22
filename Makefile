ef634905f7b62ae84474c137ba4d4b7b00922921
Evangelos Pappas <evangelos.pappas@matchesfashion.com>
2015-03-22 10:47:27 +0000

    Early commit

---
+++ Makefile
@@ -0,0 +1,38 @@
+ERL         ?= erl
+ERLC		?= erlc
+REBAR       ?= ./rebar
+EVAL        ?= "application:start(randchannel)."
+START       ?= randchannel
+NODE        ?= randchannel
+HOSTNAME    ?= `uname -n`
+REBAR       ?= "./rebar"
+CONFIG      ?= "app.config"
+RUN         := $(ERL) -pa ebin -pa deps/*/ebin -smp enable -boot start_sasl -config ${CONFIG} ${ERL_ARGS}
+
+EVAL_IGNITION   = -eval ${EVAL};
+START_IGNITION  = -s ${START}
+IGNITION        = ${START_IGNITION}
+
+all:
+	${REBAR} get-deps compile
+
+quick:
+	${REBAR} skip_deps=true compile
+
+clean:
+	${REBAR} clean
+
+quick_clean:
+	${REBAR} skip_deps=true clean
+
+package:
+	${REBAR} get-deps compile generate
+
+run: quick
+	if [ -n "${NODE}" ]; then ${RUN} -name ${NODE}@${HOSTNAME} ${IGNITION}; \
+	else ${RUN} ${IGNITION}; \
+	fi
+
+test:
+	@$(ERLC) -o tests/ tests/*.erl
+	prove -v tests/*.t
---
+++ src/encrypt_stream.erl
@@ -0,0 +1,83 @@
+%%% -*- erlang -*-
+%%%-------------------------------------------------------------------
+%%% @author Evangelos Pappas <epappas@evalonlabs.com>
+%%% @copyright (C) 2015, evalonlabs
+%%% The MIT License (MIT)
+%%%
+%%% Copyright (c) 2015 Evangelos Pappas
+%%%
+%%% Permission is hereby granted, free of charge, to any person obtaining a copy
+%%% of this software and associated documentation files (the "Software"), to deal
+%%% in the Software without restriction, including without limitation the rights
+%%% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+%%% copies of the Software, and to permit persons to whom the Software is
+%%% furnished to do so, subject to the following conditions:
+%%%
+%%% The above copyright notice and this permission notice shall be included in all
+%%% copies or substantial portions of the Software.
+%%%
+%%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+%%% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+%%% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+%%% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+%%% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+%%% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+%%% SOFTWARE.
+%%% @doc
+%%%
+%%% @end
+%%%-------------------------------------------------------------------
+
+-module(encrypt_stream).
+-author("epappas").
+
+-behaviour(gen_stream).
+
+%% API
+
+%% gen_stream callbacks
+-export([init/1, on_data/3, on_offer/3, on_state/3]).
+
+-define(SERVER, ?MODULE).
+
+-define(OPEN, open).
+-define(DROPPING, dropping).
+-define(PAUSED, paused).
+-define(STOPPED, stopped).
+-define(CLOSED, closed).
+
+-record(state, {crypto}).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+init([]) -> throw("Bad arguments, empty");
+
+init(Args) ->
+  AESKey = proplists:get_value(aesKey, Args),
+  AESIV = proplists:get_value(iv, Args),
+
+  CryptoState = crypto:stream_init(aes_ctr, AESKey, AESIV),
+
+  {ok, #state{crypto = CryptoState}}.
+
+
+on_offer(Resource, Stream, #state{crypto = CryptoState} = State) when is_list(Resource) ->
+  {NewCryptoState, CipherVal} = crypto:stream_encrypt(CryptoState, Resource),
+
+  {CipherVal, Stream, State#state{crypto = NewCryptoState}};
+
+on_offer(Resource, Stream, State) when is_list(Resource) ->
+  {undefined, Stream, State}.
+
+on_data(_Resource, Stream, State) ->
+  {ok, Stream, State}.
+
+on_state(_State, _Stream, StateData) ->
+%%   TODO maybe notify client here
+  {ok, StateData}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
